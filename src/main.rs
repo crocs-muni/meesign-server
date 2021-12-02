a1cf79cbd672118f90ff9606d91d8daa841a45b3
Antonín Dufka <xdufka1@fi.muni.cz>
2021-12-02 14:47:52 +0100

    Add tasks

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,62 @@
+use std::collections::{HashMap, VecDeque};
+
 mod rpc;
 
 mod proto {
     tonic::include_proto!("mpcoord");
 }
 
-struct State {
+pub struct State {
+    devices: HashMap<Vec<u8>, Vec<u8>>,
+    tasks: Vec<Task>,
+}
 
+impl State {
+    pub fn new() -> Self {
+        State {
+            devices: HashMap::new(),
+            tasks: Vec::new(),
+        }
+    }
+
+    pub fn add_device(&mut self, device: Vec<u8>) {
+        self.devices.insert(device, Vec::new());
+    }
+
+    pub fn add_task(&mut self, devices: &[Vec<u8>]) {
+        self.tasks.push(Task::new(devices));
+    }
+
+    pub fn get_device_tasks(&self, device: &Vec<u8>) -> Vec<u32> {
+        let mut tasks = Vec::new();
+        for (idx, task) in self.tasks.iter().enumerate() {
+            if task.subtasks.contains_key(device) {
+                tasks.push(idx as u32);
+            }
+        }
+        tasks
+    }
+
+    pub fn update_task(&mut self, task: u32, device: &Vec<u8>) {
+        self.tasks.get_mut(task as usize).unwrap().subtasks.insert(device.clone(), true);
+    }
+}
+
+pub struct Task {
+    subtasks: HashMap<Vec<u8>, bool>
+}
+
+impl Task {
+    pub fn new(devices: &[Vec<u8>]) -> Self {
+        let mut subtasks = HashMap::new();
+        for device in devices.iter() {
+            subtasks.insert(device.clone(), false);
+        }
+        Task { subtasks }
+    }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), String> {
-    rpc::run_rpc().await
+    rpc::run_rpc(State::new()).await
 }
--- src/rpc.rs
+++ src/rpc.rs
@@ -2,18 +2,28 @@ use crate::proto::*;
 use crate::proto::mpc_server::{Mpc, MpcServer};
 use tonic::{Request, Status, Response};
 use tonic::transport::Server;
+use crate::State;
+use tokio::sync::Mutex;
 
-pub struct MPCService {}
+pub struct MPCService {
+    state: Mutex<State>
+}
 
 impl MPCService {
-    pub fn new() -> Self {
-        MPCService {}
+    pub fn new(state: State) -> Self {
+        MPCService { state: Mutex::new(state) }
     }
 }
 
 #[tonic::async_trait]
 impl Mpc for MPCService {
     async fn register(&self, request: Request<RegistrationRequest>) -> Result<Response<Resp>, Status> {
+        let mut request = request.into_inner();
+        let device_id = request.device_id;
+
+        let mut state = self.state.lock().await;
+        state.add_device(device_id);
+
         let resp = Resp {
             variant: Some(resp::Variant::Success("OK".into()))
         };
@@ -22,6 +32,12 @@ impl Mpc for MPCService {
     }
 
     async fn sign(&self, request: Request<SignRequest>) -> Result<Response<Resp>, Status> {
+        let mut request = request.into_inner();
+        let device_ids = request.device_ids;
+
+        let mut state = self.state.lock().await;
+        state.add_task(&device_ids);
+
         let resp = Resp {
             variant: Some(resp::Variant::Success("OK".into()))
         };
@@ -30,25 +46,46 @@ impl Mpc for MPCService {
     }
 
     async fn get_task(&self, request: Request<TaskRequest>) -> Result<Response<Task>, Status> {
+        let mut request = request.into_inner();
+        let task_id = request.task_id;
+
         let resp = Task {
-            task_id: vec![0u8; 16]
+            task_id,
+            state: Some(task::State::Created(Vec::new()))
         };
 
         Ok(Response::new(resp))
     }
 
-    async fn update_task(&self, request: Request<Task>) -> Result<Response<Resp>, Status> {
+    async fn update_task(&self, request: Request<TaskUpdate>) -> Result<Response<Resp>, Status> {
+        let mut request = request.into_inner();
+        let task_id = request.task_id;
+        let device_id = request.device_id;
+
+        self.state.lock().await.update_task(task_id, &device_id);
+
         let resp = Resp {
             variant: Some(resp::Variant::Success("OK".into()))
         };
 
         Ok(Response::new(resp))
     }
+
+    async fn get_info(&self, request: Request<InfoRequest>) -> Result<Response<Info>, Status> {
+        let mut request = request.into_inner();
+        let device_id = request.device_id;
+
+        let resp = Info {
+            task_ids: self.state.lock().await.get_device_tasks(&device_id)
+        };
+
+        Ok(Response::new(resp))
+    }
 }
 
-pub async fn run_rpc() -> Result<(), String> {
+pub async fn run_rpc(state: State) -> Result<(), String> {
     let addr = "127.0.0.1:1337".parse().unwrap();
-    let node = MPCService::new();
+    let node = MPCService::new(state);
 
     Server::builder()
         .add_service(MpcServer::new(node))
