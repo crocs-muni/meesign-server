9b347978a0f18904ac274b516b46a37b66ea3406
Antonín Dufka <xdufka1@fi.muni.cz>
2022-04-27 16:26:13 +0200

    Move Communicator to a separate file

--- src/protocols/gg18.rs
+++ src/protocols/gg18.rs
@@ -2,78 +2,13 @@ use crate::{Task, TaskStatus, TaskType};
 use crate::proto::*;
 use prost::Message;
 use crate::group::Group;
-use crate::protocols::ProtocolType;
+use crate::protocols::{Communicator, ProtocolType};
 use crate::task::TaskResult;
 
 // TODO add logging
 
 const LAST_ROUND_GROUP: u16 = 6;
 const LAST_ROUND_SIGN: u16 = 10;
-
-struct Communicator {
-    parties: usize,
-    input: Vec<Vec<Option<Vec<u8>>>>,
-    output: Vec<Vec<u8>>,
-}
-
-impl Communicator {
-    pub fn new(parties: usize) -> Self {
-        Communicator {
-            parties,
-            input: Vec::new(),
-            output: Vec::new(),
-        }
-    }
-
-    pub fn clear_input(&mut self) {
-        self.input.clear();
-        for _ in 0..self.parties {
-            self.input.push(vec![None; self.parties]);
-        }
-    }
-
-    pub fn broadcast(&mut self, message: Vec<u8>) {
-        self.output.clear();
-
-        for _ in 0..self.parties {
-            self.output.push(message.clone());
-        }
-    }
-
-    pub fn relay(&mut self) {
-        self.output.clear();
-
-        for i in 0..self.parties {
-            let mut out: Vec<Vec<u8>> = Vec::new();
-            for j in 0..self.parties {
-                if i != j {
-                    out.push(self.input[j][i].clone().unwrap());
-                }
-            }
-            let message = Gg18Message { message: out };
-            self.output.push(message.encode_to_vec());
-        }
-    }
-
-    pub fn send_all<F>(&mut self, f: F) where F: Fn(usize) -> Vec<u8> {
-        self.output.clear();
-
-        for i in 0..self.parties {
-            self.output.push(f(i));
-        }
-    }
-
-    pub fn round_received(&self, ids: &[Vec<u8>]) -> bool {
-        ids.into_iter()
-            .zip((&self.input).into_iter())
-            .filter(|(_a, b)| b.iter().all(|x| x.is_none()))
-            .count() == 0
-    }
-
-    pub fn get_message(&self, idx: usize) -> Option<&Vec<u8>> {
-        self.output.get(idx)
-    }
-}
 
 pub struct GG18Group {
     name: String,
--- src/protocols/mod.rs
+++ src/protocols/mod.rs
@@ -1,6 +1,73 @@
 pub mod gg18;
+use prost::Message;
+use crate::proto::Gg18Message;
 
 #[derive(Copy, Clone, PartialEq, Eq)]
 pub enum ProtocolType {
     GG18,
+}
+
+struct Communicator {
+    parties: usize,
+    input: Vec<Vec<Option<Vec<u8>>>>,
+    output: Vec<Vec<u8>>,
+}
+
+impl Communicator {
+    pub fn new(parties: usize) -> Self {
+        Communicator {
+            parties,
+            input: Vec::new(),
+            output: Vec::new(),
+        }
+    }
+
+    pub fn clear_input(&mut self) {
+        self.input.clear();
+        for _ in 0..self.parties {
+            self.input.push(vec![None; self.parties]);
+        }
+    }
+
+    pub fn broadcast(&mut self, message: Vec<u8>) {
+        self.output.clear();
+
+        for _ in 0..self.parties {
+            self.output.push(message.clone());
+        }
+    }
+
+    pub fn relay(&mut self) {
+        self.output.clear();
+
+        for i in 0..self.parties {
+            let mut out: Vec<Vec<u8>> = Vec::new();
+            for j in 0..self.parties {
+                if i != j {
+                    out.push(self.input[j][i].clone().unwrap());
+                }
+            }
+            let message = Gg18Message { message: out };
+            self.output.push(message.encode_to_vec());
+        }
+    }
+
+    pub fn send_all<F>(&mut self, f: F) where F: Fn(usize) -> Vec<u8> {
+        self.output.clear();
+
+        for i in 0..self.parties {
+            self.output.push(f(i));
+        }
+    }
+
+    pub fn round_received(&self, ids: &[Vec<u8>]) -> bool {
+        ids.into_iter()
+            .zip((&self.input).into_iter())
+            .filter(|(_a, b)| b.iter().all(|x| x.is_none()))
+            .count() == 0
+    }
+
+    pub fn get_message(&self, idx: usize) -> Option<&Vec<u8>> {
+        self.output.get(idx)
+    }
 }
