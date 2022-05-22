83cbab2167b518896e207e787985bfbb3a57d4b4
Antonín Dufka <xdufka1@fi.muni.cz>
2022-05-22 21:24:08 +0200

    Add get_devices command

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ mod proto {
 use crate::state::State;
 use clap::{Parser, Subcommand};
 use crate::proto::mpc_client::MpcClient;
+use std::time::SystemTime;
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
@@ -32,6 +33,7 @@ enum Commands {
         identifier: String,
         name: String
     },
+    GetDevices,
 }
 
 async fn register(server: String, identifier: &[u8], name: &str) -> Result<(), String> {
@@ -62,6 +64,28 @@ async fn register(server: String, identifier: &[u8], name: &str) -> Result<(), S
     Ok(())
 }
 
+async fn get_devices(server: String) -> Result<(), String> {
+    let mut client = MpcClient::connect(server)
+        .await
+        .map_err(|_| String::from("Unable to connect to server"))?;
+
+    let request = tonic::Request::new(crate::proto::DevicesRequest {});
+
+    let mut response = client.get_devices(request)
+        .await
+        .map_err(|_| String::from("Request failed"))?
+        .into_inner();
+
+    let now = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs();
+
+    response.devices.sort_by_key(|x| u64::MAX - x.last_active);
+    for device in response.devices {
+        println!("{} [{}] (seen before {}s)", &device.name, hex::encode(device.identifier), now - device.last_active);
+    }
+
+    Ok(())
+}
+
 #[tokio::main]
 async fn main() -> Result<(), String> {
     env_logger::init();
@@ -74,6 +98,7 @@ async fn main() -> Result<(), String> {
                 let identifier = hex::decode(identifier).unwrap();
                 register(server, &identifier, &name).await
             },
+            Commands::GetDevices => get_devices(server).await
         }
     } else {
         rpc::run_rpc(State::new(), &args.addr, args.port).await
