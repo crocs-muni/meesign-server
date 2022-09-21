dd14afdca755ad74e7a1ea4f5e4438e6f30599bf
Kristian Mika <kristian.mika@protonmail.com>
2022-09-21 21:32:09 +0200

    chore: generate certificate into a dedicated key folder

--- generate_certificates.sh
+++ generate_certificates.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-cat > cert.conf << EOT
+
+KEY_FOLDER="key"
+mkdir --parent "./${KEY_FOLDER}"
+
+cat > "./${KEY_FOLDER}/cert.conf" << EOT
 [req]
 distinguished_name = req_distinguished_name
 req_extensions = v3_req
@@ -18,6 +22,7 @@ subjectKeyIdentifier = hash
 keyUsage = critical, cRLSign, digitalSignature, keyCertSign
 EOT
 
-openssl ecparam -name prime256v1 -genkey -noout -out server-key.pem
-openssl req -new -x509 -key server-key.pem -out server-cert.pem -days 1461 -config cert.conf -nodes -extensions v3_req
-rm cert.conf
+
+openssl ecparam -name prime256v1 -genkey -noout -out "./${KEY_FOLDER}/server-key.pem"
+openssl req -new -x509 -key "./${KEY_FOLDER}/server-key.pem" -out "./${KEY_FOLDER}/server-cert.pem" -days 1461 -config "./${KEY_FOLDER}/cert.conf" -nodes -extensions v3_req
+rm "./${KEY_FOLDER}/cert.conf"
