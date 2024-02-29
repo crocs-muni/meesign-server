6a9811dc8958127b704996bf17b45e9dbdaa2f5e
Antonín Dufka <xdufka1@fi.muni.cz>
2024-02-29 14:51:55 +0100

    feat: make identifier truncation in logs configurable by `TRUNC` env variable

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,12 @@
 pub fn hextrunc<T: AsRef<[u8]>>(s: T) -> String {
-    format!("{}...", hex::encode(&s.as_ref()[..4]))
+    let trunc_len = std::env::var("TRUNC")
+        .ok()
+        .as_ref()
+        .and_then(|x| x.parse().ok())
+        .unwrap_or(0);
+    if trunc_len == 0 {
+        hex::encode(s.as_ref())
+    } else {
+        format!("{}...", hex::encode(&s.as_ref()[..trunc_len]))
+    }
 }
