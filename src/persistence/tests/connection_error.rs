ca7df7bf42fefe9437ffe8b05d61f6e264f8087d
Kristian Mika <kristian.mika@protonmail.com>
2024-04-03 10:45:50 +0200

    refactor: implement TestContextError using ThisError

--- src/persistence/tests/connection_error.rs
+++ src/persistence/tests/connection_error.rs
@@ -1,32 +1,9 @@
-use std::{error::Error, fmt::Display};
+use thiserror::Error;
 
-use diesel::QueryResult;
-
-#[derive(Debug)]
-pub struct TestContextError {}
-
-impl Error for TestContextError {}
-
-impl Display for TestContextError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Connection error")
-    }
-}
-
-impl From<QueryResult<usize>> for TestContextError {
-    fn from(_value: QueryResult<usize>) -> Self {
-        Self {}
-    }
-}
-
-impl From<diesel::result::Error> for TestContextError {
-    fn from(_value: diesel::result::Error) -> Self {
-        Self {}
-    }
-}
-
-impl From<diesel::ConnectionError> for TestContextError {
-    fn from(_value: diesel::ConnectionError) -> Self {
-        Self {}
-    }
+#[derive(Error, Debug)]
+pub enum TestContextError {
+    #[error("diesel error: {0}")]
+    DieselError(#[from] diesel::result::Error),
+    #[error("diesel connection error: {0}")]
+    ConnectionError(#[from] diesel::ConnectionError),
 }
