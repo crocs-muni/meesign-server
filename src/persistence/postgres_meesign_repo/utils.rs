6d4f63db18b70ea1ce570008a0a703dbe03a2cc5
Kristian Mika <kristian.mika@protonmail.com>
2024-04-03 10:46:38 +0200

    fix: correctly validate the username

--- src/persistence/postgres_meesign_repo/utils.rs
+++ src/persistence/postgres_meesign_repo/utils.rs
@@ -1,13 +1,15 @@
+const MAX_USERNAME_LENGTH: usize = 64;
+
 pub(crate) trait NameValidator {
     fn is_name_valid(&self) -> bool;
 }
 
 impl NameValidator for &str {
     fn is_name_valid(&self) -> bool {
-        self.chars().count() > 64
-            || self
+        self.chars().count() <= MAX_USERNAME_LENGTH
+            && !self
                 .chars()
                 .any(|x| x.is_ascii_punctuation() || x.is_control())
-            || self.is_empty()
+            && !self.is_empty()
     }
 }
