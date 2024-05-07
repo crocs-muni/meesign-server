bb5185422c36323bffcb3bcc5556a575f4533e34
Kristian Mika <kristian.mika@protonmail.com>
2024-05-07 12:36:14 +0200

    feat: enable duplicate device names

--- migrations/2023-09-14-110756_create_device/up.sql
+++ migrations/2023-09-14-110756_create_device/up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE device (
     "id" bytea PRIMARY KEY,
-    "name" varchar UNIQUE NOT NULL,
+    "name" varchar NOT NULL,
     "kind" device_kind NOT NULL,
     "certificate" bytea NOT NULL,
     "last_active" timestamptz NOT NULL DEFAULT NOW()
