fb0ef9911c15acdb4239dca771df1e2d8986b87f
Antonín Dufka <xdufka1@fi.muni.cz>
2024-05-21 17:45:31 +0200

    fix: check for too short inputs in truncation

--- src/utils.rs
+++ src/utils.rs
@@ -4,7 +4,7 @@ pub fn hextrunc<T: AsRef<[u8]>>(s: T) -> String {
         .as_ref()
         .and_then(|x| x.parse().ok())
         .unwrap_or(0);
-    if trunc_len == 0 {
+    if s.as_ref().len() <= trunc_len {
         hex::encode(s.as_ref())
     } else {
         format!("{}...", hex::encode(&s.as_ref()[..trunc_len]))
