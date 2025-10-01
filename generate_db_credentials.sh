7a1d99e1479e4317593b32a001dd2c89819fb3e3
quapka <quapka@gmail.com>
2025-10-01 10:37:58 +0200

    don't backup non-existent .env file

--- generate_db_credentials.sh
+++ generate_db_credentials.sh
@@ -11,19 +11,22 @@ ENV_FILE='./.env'
 ENV_FILE_BACKUP="$ENV_FILE.bak"
 FORCE_FLAG='--force'
 
-if test -f "$ENV_FILE" && test "$1" != "$FORCE_FLAG"; then
-    echo "The '$ENV_FILE' file exists and the '$FORCE_FLAG' command line flag was not used."
-    echo "Doing nothing."
-    exit 0
+if test -f "$ENV_FILE"; then
+    if test "$1" != "$FORCE_FLAG"; then
+        echo "The '$ENV_FILE' file exists and the '$FORCE_FLAG' command line flag was not used."
+        echo "Doing nothing."
+        exit 2
+    fi
+
+    echo "Archiving previous '$ENV_FILE' in '$ENV_FILE_BACKUP'."
+    mv "$ENV_FILE" "$ENV_FILE_BACKUP"
 fi
 
-echo "Archiving previous '$ENV_FILE' in '$ENV_FILE_BACKUP'."
-mv "$ENV_FILE" "$ENV_FILE_BACKUP"
-
-echo "Storing new database credentails in '$ENV_FILE'."
 # create a new file with 600 permissions
 install --mode 600 /dev/null "$ENV_FILE"
+
 # store db credentials with freshly generated password
+echo "Storing new database credentails in '$ENV_FILE'."
 cat <<EOF > "$ENV_FILE"
 POSTGRES_USER="meesign"
 POSTGRES_PASSWORD="$(openssl rand -hex 32)"
