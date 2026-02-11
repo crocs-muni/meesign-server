7cfa624bfb348221ad206752f44756a0ff660dae
Marek Mracna <marek.mracna07@gmail.com>
2026-02-11 23:09:56 +0100

    feat: add mock TaskStore

--- src/task_store.rs
+++ src/task_store.rs
@@ -23,3 +23,36 @@ pub trait TaskStore {
     /// does not reference an existing `Task`
     async fn get_task_mut(&self, task_id: &Uuid) -> Result<Self::TaskRefMut, Error>;
 }
+
+#[cfg(test)]
+use mockall::mock;
+
+#[cfg(test)]
+mock! {
+    pub TaskStore {
+        pub fn persist_task(&self, task: VotingTask) ->
+            Result<Option<Task>, Error>;
+        pub fn get_task(&self, task_id: &Uuid) ->
+            Result<<Self as TaskStore>::TaskRef, Error>;
+        pub fn get_task_mut(&self, task_id: &Uuid) ->
+            Result<<Self as TaskStore>::TaskRefMut, Error>;
+    }
+}
+
+// TODO: Mock the async behavior as well.
+#[cfg(test)]
+#[async_trait]
+impl TaskStore for MockTaskStore {
+    type TaskRef = Box<Task>;
+    type TaskRefMut = Box<Task>;
+
+    async fn persist_task(&self, task: VotingTask) -> Result<Option<Task>, Error> {
+        self.persist_task(task)
+    }
+    async fn get_task(&self, task_id: &Uuid) -> Result<Self::TaskRef, Error> {
+        self.get_task(task_id)
+    }
+    async fn get_task_mut(&self, task_id: &Uuid) -> Result<Self::TaskRefMut, Error> {
+        self.get_task_mut(task_id)
+    }
+}
